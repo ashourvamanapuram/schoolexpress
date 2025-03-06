577c9ced729b3b5a8af17ba14a0a08693896f862
ashourvamanapuram <ashourvamanapuram@gmail.com>
2025-03-06 22:38:55 +0000

    Add 2.ts

---
+++ src/2.ts
@@ -0,0 +1,8 @@
+export class Project {
+  public id: string;
+  public name: string;
+  public description: string;
+  public category: string;
+  public url: string;
+  public dateCreated: Date;
+}
