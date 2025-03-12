918d3fb8710cd648239c56a23cf251c1d09b9093
ashourvamanapuram <ashourvamanapuram@gmail.com>
2025-03-12 08:54:35 +0000

    Add 8.ts

---
+++ src/8.ts
@@ -0,0 +1,10 @@
+
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-schoolexpress',
+  templateUrl: './schoolexpress.component.html',
+  styleUrls: ['./schoolexpress.component.css']
+})
+export class SchoolexpressComponent {
+  constructor() {}
+}
