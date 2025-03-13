a265b7720d4e95c05913532ec6cd262828911516
ashourvamanapuram <ashourvamanapuram@gmail.com>
2025-03-13 11:40:53 +0000

    Add 9.ts

---
+++ src/9.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express';
+
+const getStudents = async (req: Request, res: Response) => {
+  try {
+    const students = await Student.find();
+    res.json(students);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
